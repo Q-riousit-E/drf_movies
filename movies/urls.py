0a119a5c5ea4f0758dbc7d99ff1eff5e5e3e920c
SeongjaePark <gndan4@gmail.com>
2021-05-21 13:34:17 +0900

    feat: Add an end point to GET movie detail

--- movies/urls.py
+++ movies/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('genres/<int:genre_number>/movie/', views.genre_add_movie),
     path('movies/', views.movie_list),
     path('movies/index/', views.movie_index),
+    path('movies/<int:movie_pk>/', views.movie_detail),
     path('articles/', views.article_list),
     path('articles/<int:article_pk>/', views.article_detail),
     path('articles/<int:article_pk>/comments/', views.create_comment),
--- movies/views.py
+++ movies/views.py
@@ -86,6 +86,15 @@ def movie_index(request):
     return JsonResponse(data)
 
 
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def movie_detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
+
+
 @api_view(['GET', 'POST'])
 @authentication_classes([])
 @permission_classes([])
