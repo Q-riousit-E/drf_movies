30a1f0566f93ca7bf0ea8b55a455092703480304
SeongjaePark <gndan4@gmail.com>
2021-05-24 13:11:21 +0900

    feat: Register GenreAdmin

--- movies/admin.py
+++ movies/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Genre
 
 
 class MovieAdmin(admin.ModelAdmin):
     pass
 
 
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(Genre, GenreAdmin)
