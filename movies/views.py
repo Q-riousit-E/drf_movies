c0eaa8fe4ebcbc342f762e6d01a894839f6432c7
SeongjaePark <gndan4@gmail.com>
2021-05-27 15:37:19 +0900

    fix: Add comment_set in article object in review-sets endpoint

--- movies/views.py
+++ movies/views.py
@@ -249,6 +249,8 @@ def review_set_list(request, movie_pk):
         if article:
             article_dict = model_to_dict(article)
             article_dict['comment_count'] = article.comments.count()
+            article_dict['comment_set'] = list(model_to_dict(
+                comment) for comment in article.comments.all())
         if not detailed and not article:
             continue
         data.append({
